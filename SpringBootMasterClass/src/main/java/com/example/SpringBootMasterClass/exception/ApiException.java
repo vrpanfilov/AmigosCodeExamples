84c539db746fb91bae447385a7d9786c94ebef2c
Vladimir Panfilov <vrpanfilov@yandex.ru>
2022-11-09 09:38:31 +0300

    record added to SpringBootMasterClass.

--- SpringBootMasterClass/src/main/java/com/example/SpringBootMasterClass/exception/ApiException.java
+++ SpringBootMasterClass/src/main/java/com/example/SpringBootMasterClass/exception/ApiException.java
@@ -2,16 +2,15 @@ package com.example.SpringBootMasterClass.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.ToString;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-@AllArgsConstructor
-@Data
-public class ApiException {
-    private final String message;
-    private final Throwable throwable;
-    private final HttpStatus httpStatus;
-    private final ZonedDateTime zonedDateTime;
+// private final, getter, public constructor, equals, hashcode,
+// and to string
+public record ApiException(
+        String message,
+        Throwable throwable,
+        HttpStatus httpStatus,
+        ZonedDateTime zonedDateTime) {
 }
