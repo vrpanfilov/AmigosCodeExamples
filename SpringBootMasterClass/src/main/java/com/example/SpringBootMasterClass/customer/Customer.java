46499e6fff98d59047a3c170f96c3cfde23cbf29
Vladimir Panfilov <vrpanfilov@yandex.ru>
2022-11-09 09:17:19 +0300

    Lombok added to SpringBootMasterClass.

--- SpringBootMasterClass/src/main/java/com/example/SpringBootMasterClass/customer/Customer.java
+++ SpringBootMasterClass/src/main/java/com/example/SpringBootMasterClass/customer/Customer.java
@@ -2,6 +2,10 @@ package com.example.SpringBootMasterClass.customer;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -11,6 +15,10 @@ import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Customer {
     @Id
     private Long id;
@@ -22,18 +30,6 @@ public class Customer {
     private String password;
     @Email
     private String email;
-
-    public Customer() {
-    }
-
-    Customer(Long id,
-             String name,
-             String password, String email) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-        this.email = email;
-    }
 
     @JsonProperty("customer_id")
     public Long getId() {
@@ -53,13 +49,4 @@ public class Customer {
         return email;
     }
 
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
-}
+ }
---
+++ SpringBootMasterClass/src/test/java/com/example/SpringBootMasterClass/customer/CustomerServiceTest.java
@@ -0,0 +1,69 @@
+package com.example.SpringBootMasterClass.customer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class CustomerServiceTest {
+    @Autowired
+    private CustomerRepository customerRepository;
+    private CustomerService underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new CustomerService(customerRepository);
+    }
+
+    @AfterEach
+    void tearDown() {
+        customerRepository.deleteAll();
+    }
+
+    @Test
+    void getCustomers() {
+        // given
+        Customer jamila = new Customer(1L,
+                "Jamila",
+                "hello",
+                "jamila@gmail.com");
+        Customer ali = new Customer(2L,
+                "Ali",
+                "hello",
+                "ali@gmail.com");
+        customerRepository.saveAll(Arrays.asList(jamila, ali));
+
+        // when
+        List<Customer> customers = underTest.getCustomers();
+
+        //then
+        assertEquals(2, customers.size());
+    }
+
+    @Test
+    void getCustomer() {
+        //given
+        Customer jamila = new Customer(1L,
+                "Jamila",
+                "hello",
+                "jamila@gmail.com");
+
+        customerRepository.save(jamila);
+
+        //when
+        Customer actual = underTest.getCustomer(1L);
+
+        // then
+        assertEquals(1L, actual.getId());
+        assertEquals("Jamila", actual.getName());
+        assertEquals("hello", actual.getPassword());
+        assertEquals("jamila@gmail.com", actual.getEmail());
+    }
+}
