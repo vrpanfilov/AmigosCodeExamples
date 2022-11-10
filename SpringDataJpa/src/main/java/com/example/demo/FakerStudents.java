370f850a385454d297c1ecd398e7d30a2f9be7f0
Vladimir Panfilov <vrpanfilov@yandex.ru>
2022-11-10 20:50:53 +0300

    Made some beans in SpringDataJpa.

---
+++ SpringDataJpa/src/main/java/com/example/demo/FakerStudents.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FakerStudents {
+    @Bean
+    CommandLineRunner fakerStudentsRunner(StudentRepository studentRepository) {
+        return args -> {
+            generateRamdomStudents(studentRepository);
+            sorting(studentRepository);
+            paging(studentRepository);
+        };
+    }
+
+    private void generateRamdomStudents(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+        for (int i = 0; i < 20; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@amigoscode.edu",
+                    firstName, lastName);
+            Student student = new Student(null,
+                    firstName,
+                    lastName,
+                    email,
+                    faker.number().numberBetween(17, 28));
+            studentRepository.save(student);
+        }
+    }
+
+    private void sorting(StudentRepository studentRepository) {
+//            Sort sort = Sort.by(Sort.Direction.DESC, "firstName");
+        Sort sort = Sort.by("firstName").ascending()
+                .and(Sort.by("age").descending());
+        sort = Sort.by("age").descending()
+                .and(Sort.by("firstName").ascending());
+        studentRepository.findAll(sort)
+                .forEach(student -> System.out.println(
+                        student.getFirstName() + " " + student.getAge()));
+    }
+
+    private void paging(StudentRepository studentRepository) {
+        int pageNumber = 0;
+        int pageSize = 5;
+        boolean hasNextPage = true;
+        while (hasNextPage) {
+            PageRequest pageRequest = PageRequest.of(
+                    pageNumber++,
+                    pageSize,
+                    Sort.by("firstName").descending());
+            Page<Student> page = studentRepository.findAll(pageRequest);
+            System.out.println(page);
+            page.getContent().forEach(System.out::println);
+            hasNextPage = page.hasNext();
+        }
+    }
+}
---
+++ SpringDataJpa/src/main/java/com/example/demo/LocalStudents.java
@@ -0,0 +1,164 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class LocalStudents {
+//    @Bean
+    CommandLineRunner localStudentsRunner(StudentRepository studentRepository) {
+        return args -> {
+            createStudents(studentRepository);
+//            repositoryMethods(studentRepository);
+//            jpaQueryMethods(studentRepository);
+//            nativeQueryMethods(studentRepository);
+//            namedParameters(studentRepository);
+            modifyingMethods(studentRepository);
+        };
+    }
+
+    private void createStudents(StudentRepository studentRepository) {
+        Student maria = new Student(null,
+                "Maria",
+                "Jones",
+                "maria.jones@amigoscode.edu",
+                21
+        );
+        Student ahmed = new Student(null,
+                "Ahmed",
+                "Ali",
+                "ahmed.ali@amigoscode.edu",
+                18
+        );
+        Student jamal = new Student(null,
+                "Jamal",
+                "Bond",
+                "jamal.bond@gmail.com",
+                32);
+        Student maria2 = new Student(null,
+                "Maria",
+                "Jones",
+                "maria2.jones@amigoscode.edu",
+                25
+        );
+
+        System.out.println("Adding maria, ahmed, jamal and maria2");
+        studentRepository.saveAll(List.of(maria, ahmed, jamal, maria2));
+
+        System.out.println();
+    }
+
+    private void repositoryMethods(StudentRepository studentRepository) {
+        System.out.println("Number of students: " + studentRepository.count());
+
+        System.out.println();
+        studentRepository.findById(2L).ifPresentOrElse(System.out::println,
+                () -> System.out.println("Student with id 2 not found"));
+
+        System.out.println();
+        studentRepository.findById(4L).ifPresentOrElse(System.out::println,
+                () -> System.out.println("Student with id 4 not found"));
+
+        System.out.println();
+        System.out.println("Select all students");
+        studentRepository.findAll().forEach(System.out::println);
+
+        System.out.println();
+        System.out.println("Delete maria");
+        studentRepository.deleteStudentById(1L);
+
+        System.out.println();
+        System.out.println("Number of students: " + studentRepository.count());
+    }
+
+    private void jpaQueryMethods(StudentRepository studentRepository) {
+        String email = "ahmed.ali@amigoscode.edu";
+        studentRepository.findByEmail(email)
+                .ifPresentOrElse(
+                        student -> {
+                            System.out.println("Student with email '" + email + "':");
+                            System.out.println(student);
+                        },
+                        () -> System.out.println(
+                                "Student with email " + email + " not found"));
+
+        System.out.println();
+        String wrongEmail = "email@gmail.com";
+        studentRepository.findByEmail(wrongEmail)
+                .ifPresentOrElse(
+                        student -> {
+                            System.out.println("Student with email '" + wrongEmail + "':");
+                            System.out.println(student);
+                        },
+                        () -> System.out.println(
+                                "Student with email '" + wrongEmail + "' not found"));
+
+        System.out.println();
+        String firstName = "Ahmed";
+        Integer age = 18;
+        studentRepository.findByFirstNameAndAge(firstName, age)
+                .ifPresentOrElse(
+                        student -> {
+                            System.out.println("Student " + firstName + " of age " + age + " found:");
+                            System.out.println(student);
+                        },
+                        () -> System.out.println(
+                                "Student with firstName " + firstName +
+                                        " and of age " + age + " not found"));
+        System.out.println();
+        Integer wrongAge = 28;
+        studentRepository.findByFirstNameAndAge(firstName, wrongAge)
+                .ifPresentOrElse(
+                        student -> {
+                            System.out.println("Student found:");
+                            System.out.println(student);
+                        },
+                        () -> System.out.println(
+                                "Student with firstName " + firstName +
+                                        " and of age " + wrongAge + " not found"));
+
+        System.out.println();
+        Integer thresholdAge = 20;
+        System.out.println("Students older than " + thresholdAge + ":");
+        studentRepository.findByAgeIsGreaterThan(thresholdAge).forEach(System.out::println);
+
+        // Это работает неверно, потому что в репозитории @Query
+        // перекрывает именной запрос
+        System.out.println();
+        System.out.println("Next works wrong!");
+        Integer theAge = 21;
+        System.out.println("Students older than or equal to" + theAge + ":");
+        studentRepository.findByAgeGreaterThanEqual(theAge).forEach(System.out::println);
+    }
+
+    private void nativeQueryMethods(StudentRepository studentRepository) {
+        String firstName = "Maria";
+        Integer age = 22;
+        System.out.println("Marias of age >= " + age);
+        studentRepository.findByFirstNameAndAgeGreaterThanEqualNative(firstName, age)
+                .forEach(System.out::println);
+
+        System.out.println();
+        Integer newAge = 20;
+        System.out.println("Marias of age >= " + newAge);
+        studentRepository.findByFirstNameAndAgeGreaterThanEqualNative(firstName, newAge)
+                .forEach(System.out::println);
+    }
+
+    private void namedParameters(StudentRepository studentRepository) {
+        String firstName = "Maria";
+        Integer age = 21;
+        System.out.println("Marias of age >= " + age);
+        studentRepository.findByFirstNameAndAgeNamed(firstName, age)
+                .forEach(System.out::println);
+    }
+
+    private void modifyingMethods(StudentRepository studentRepository) {
+        Long id = 4L;
+        System.out.println("Delete the student with id " + id);
+        int count = studentRepository.deleteStudentById(id);
+        System.out.println("Deleted " + count + " rows");
+    }
+}
