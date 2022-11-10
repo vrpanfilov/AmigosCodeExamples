c44421af43587a68e8f7114160f3df77ec502873
Vladimir Panfilov <vrpanfilov@yandex.ru>
2022-11-10 10:23:29 +0300

    JPQL query methods in SpringDataJpa.

--- SpringDataJpa/src/main/java/com/example/demo/Application.java
+++ SpringDataJpa/src/main/java/com/example/demo/Application.java
@@ -37,31 +37,88 @@ public class Application {
                     32);
 
             System.out.println("Adding maria, ahmed and jamal");
-            studentRepository.saveAll(List.of(maria, ahmed,
-                    jamal));
+            studentRepository.saveAll(List.of(maria, ahmed, jamal));
 
-            System.out.println();
-            System.out.println("Number of students: " + studentRepository.count());
-
-
-            System.out.println();
-            studentRepository.findById(2L).ifPresentOrElse(System.out::println,
-                    () -> System.out.println("Student with id 2 not found"));
-
-            System.out.println();
-            studentRepository.findById(4L).ifPresentOrElse(System.out::println,
-                    () -> System.out.println("Student with id 4 not found"));
-
-            System.out.println();
-            System.out.println("Select all students");
-            studentRepository.findAll().forEach(System.out::println);
-
-            System.out.println();
-            System.out.println("Delete maria");
-            studentRepository.deleteById(1L);
-
-            System.out.println();
-            System.out.println("Number of students: " + studentRepository.count());
+//            repositoryMethods(studentRepository);
+            queryMethods(studentRepository);
         };
+    }
+
+    private void queryMethods(StudentRepository studentRepository) {
+        String email = "ahmed.ali@amigoscode.edu";
+        System.out.println();
+        studentRepository.findByEmail(email)
+                .ifPresentOrElse(
+                        student -> {
+                            System.out.println("Student with email '" + email + "':");
+                            System.out.println(student);
+                        },
+                        () -> System.out.println(
+                                "Student with email " + email + " not found"));
+
+        System.out.println();
+        String wrongEmail = "email@gmail.com";
+        studentRepository.findByEmail(wrongEmail)
+                .ifPresentOrElse(
+                        student -> {
+                            System.out.println("Student with email '" + wrongEmail + "':");
+                            System.out.println(student);
+                        },
+                        () -> System.out.println(
+                                "Student with email '" + wrongEmail + "' not found"));
+
+        System.out.println();
+        String firstName = "Ahmed";
+        Integer age = 18;
+        studentRepository.findByFirstNameAndAge(firstName, age)
+                .ifPresentOrElse(
+                        student -> {
+                            System.out.println("Student " + firstName + " of age " + age + " found:");
+                            System.out.println(student);
+                        },
+                        () -> System.out.println(
+                                "Student with firstName " + firstName +
+                                        " and of age " + age + " not found"));
+        System.out.println();
+        Integer wrongAge = 28;
+        studentRepository.findByFirstNameAndAge(firstName, wrongAge)
+                .ifPresentOrElse(
+                        student -> {
+                            System.out.println("Student found:");
+                            System.out.println(student);
+                        },
+                        () -> System.out.println(
+                                "Student with firstName " + firstName +
+                                        " and of age " + wrongAge + " not found"));
+
+        System.out.println();
+        Integer thresholdAge = 20;
+        System.out.println("Students older than " + thresholdAge + ":");
+        studentRepository.findByAgeIsGreaterThan(thresholdAge).forEach(System.out::println);
+    }
+
+    private void repositoryMethods(StudentRepository studentRepository) {
+        System.out.println();
+        System.out.println("Number of students: " + studentRepository.count());
+
+
+        System.out.println();
+        studentRepository.findById(2L).ifPresentOrElse(System.out::println,
+                () -> System.out.println("Student with id 2 not found"));
+
+        System.out.println();
+        studentRepository.findById(4L).ifPresentOrElse(System.out::println,
+                () -> System.out.println("Student with id 4 not found"));
+
+        System.out.println();
+        System.out.println("Select all students");
+        studentRepository.findAll().forEach(System.out::println);
+
+        System.out.println();
+        System.out.println("Delete maria");
+        studentRepository.deleteById(1L);
+
+        System.out.println();
+        System.out.println("Number of students: " + studentRepository.count());
     }
 }
--- SpringDataJpa/src/main/java/com/example/demo/StudentRepository.java
+++ SpringDataJpa/src/main/java/com/example/demo/StudentRepository.java
@@ -2,5 +2,14 @@ package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
+
+    Optional<Student> findByFirstNameAndAge(
+            String firstName, Integer age);
+
+    <S extends Student> List<S> findByAgeIsGreaterThan(Integer age);
 }
