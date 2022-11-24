cb6752216fbefde1545af562f9abbe0579a43cbd
Vladimir Panfilov <vrpanfilov@yandex.ru>
2022-11-24 19:41:51 +0300

    SoftwareTesting 1-starting-pont

--- software-testing/src/main/java/com/amigoscode/testing/payment/PaymentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.amigoscode.testing.payment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
