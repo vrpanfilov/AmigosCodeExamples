2e9adeae13ea698a749cdd5be6c0e902a2f70e7d
Vladimir Panfilov <vrpanfilov@yandex.ru>
2022-11-24 19:43:03 +0300

    SoftwareTesting 1-starting-pont

---
+++ SoftwareTesting/src/main/java/pvr/SoftwareTesting/SoftwareTestingApplication.java
@@ -0,0 +1,13 @@
+package pvr.SoftwareTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareTestingApplication.class, args);
+	}
+
+}
---
+++ SoftwareTesting/src/test/java/pvr/SoftwareTesting/SoftwareTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package pvr.SoftwareTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
