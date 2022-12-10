15655372e2cce933801e895edce203f5ee6d41cd
Vladimir Panfilov <vrpanfilov@yandex.ru>
2022-12-10 23:38:50 +0300

    amgosservicies. Customer communicate with 1 Fraud.

--- amigosservicies/customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ amigosservicies/customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -2,12 +2,14 @@ package com.amigoscode.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -17,7 +19,16 @@ public class CustomerService {
                 .build();
         // todo: check if email valid
         // todo: check if email taken
-
         customerRepository.saveAndFlush(customer);
+        // todo: check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId());
+
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+        // todo: send notification
     }
 }
--- amigosservicies/fraud/src/main/java/com/amigoscode/fraud/FraudController.java
+++ amigosservicies/fraud/src/main/java/com/amigoscode/fraud/FraudController.java
@@ -1,11 +1,13 @@
 package com.amigoscode.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/fraud-check")
 @AllArgsConstructor
@@ -18,6 +20,7 @@ public class FraudController {
             @PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService
                 .isFraudulentCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
