11567cbc957663ce209a12136da918dcfe41d100
Vladimir Panfilov <vrpanfilov@yandex.ru>
2022-12-27 01:48:15 +0300

    amigosservices. Working with AMQP from commandLineRunner in NotificationApplication.

--- amigosservicies/notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
+++ amigosservicies/notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
@@ -1,13 +1,35 @@
 package com.amigoscode.notification;
 
+import com.amigoscode.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
-@EnableEurekaClient
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.amigoscode.notification",
+                "com.amigoscode.amqp"
+        }
+)
+//@EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+    CommandLineRunner commandLineRunner(
+            RabbitMQMessageProducer producer,
+            NotificationConfig notificationConfig
+    ) {
+        return args -> {
+            producer.publish(
+                    new Person("Ali", 18),
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey()
+            );
+        };
+    }
+
+    record Person(String name, int age) {}
 }
